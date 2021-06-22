8292faf69f432a4242d2ddff5c764d229aca5f7e
LiuyHq <yaoliu.yao@alibaba-inc.com>
2021-06-22 17:58:05 +0800

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ package com.alibaba.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        System.out.println("change in 2021/6/22 17:57");
     }
 }
